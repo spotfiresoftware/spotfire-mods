c9d7f9ada770b921bf3ed300f5ae62f95a3dec6d
Olof Bjerke <objerke@tibco.com>
2021-04-22 05:32:26 +0200

    Formatting declaration files

--- examples/js-dev-starter/spotfire/spotfire-api-1-1.d.ts
+++ examples/js-dev-starter/spotfire/spotfire-api-1-1.d.ts
@@ -1,8 +1,8 @@
 /*
-* Copyright © 2020. TIBCO Software Inc.
-* This file is subject to the license terms contained
-* in the license file that is distributed with this file.
-*/
+ * Copyright © 2020. TIBCO Software Inc.
+ * This file is subject to the license terms contained
+ * in the license file that is distributed with this file.
+ */
 export as namespace Spotfire;
 
 /**
@@ -16,7 +16,8 @@ export declare type AbortPredicate = (currentRowCount: number) => boolean;
  * These can be either document properties, data table properties or data column properties.
  * @public
  */
-export declare interface AnalysisProperty<T extends AnalysisPropertyDataType = AnalysisPropertyDataType> extends AnalysisPropertyValue<T> {
+export declare interface AnalysisProperty<T extends AnalysisPropertyDataType = AnalysisPropertyDataType>
+    extends AnalysisPropertyValue<T> {
     /**
      * Set the value of this instance.
      * @param value - The value to set.
@@ -256,7 +257,19 @@ export declare interface DataType {
     /**
      * Gets the name of this DataType.
      */
-    name: "String" | "Integer" | "LongInteger" | "Real" | "SingleReal" | "Currency" | "Boolean" | "Date" | "DateTime" | "Time" | "TimeSpan" | "Binary";
+    name:
+        | "String"
+        | "Integer"
+        | "LongInteger"
+        | "Real"
+        | "SingleReal"
+        | "Currency"
+        | "Boolean"
+        | "Date"
+        | "DateTime"
+        | "Time"
+        | "TimeSpan"
+        | "Binary";
     /**
      * Gets a value indicating whether the data type is numeric or not, that is,
      * Integer, Currency, Real, LongInteger, or SingleReal.
@@ -716,7 +729,9 @@ export declare interface ErrorOverlay {
  * @public
  */
 export declare type ExtractValueType<readableArray extends ReadonlyArray<Readable<any>>> = {
-    [readableName in keyof readableArray]: readableArray[readableName] extends Readable<infer readableNameType> ? readableNameType : never;
+    [readableName in keyof readableArray]: readableArray[readableName] extends Readable<infer readableNameType>
+        ? readableNameType
+        : never;
 };
 
 /**
@@ -1307,7 +1322,8 @@ export declare interface Readable<T = any> extends Promise<T> {
  * A full node will be created by using a {@link Reader}.
  * @public
  */
-export declare type ReadableProxy<Node> = Readable<Node> & Omit<Pick<Node, MethodKeys<Node>>, OmittedReadableProxyMethods>;
+export declare type ReadableProxy<Node> = Readable<Node> &
+    Omit<Pick<Node, MethodKeys<Node>>, OmittedReadableProxyMethods>;
 
 /**
  * The reader is responsible for combining multiple {@link Readable}s and scheduling a callback to be invoked
@@ -1458,7 +1474,9 @@ export declare interface SpotfireDocument {
     /**
      * Provides access to the `Document Property` with the specified `name` in the Spotfire Document.
      */
-    property<T extends AnalysisPropertyDataType = AnalysisPropertyDataType>(name: string): ReadableProxy<AnalysisProperty<T>>;
+    property<T extends AnalysisPropertyDataType = AnalysisPropertyDataType>(
+        name: string
+    ): ReadableProxy<AnalysisProperty<T>>;
     /**
      * Provides access to the {@link Page}s in the Spotfire document.
      */
@@ -1578,4 +1596,4 @@ export declare interface Tooltip {
     hide(): void;
 }
 
-export { }
+export {};
