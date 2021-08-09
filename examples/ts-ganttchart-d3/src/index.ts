97eb088c9f931d66f94e549d5da002b3a84dd393
Olof Bjerke <objerke@tibco.com>
2021-08-09 13:27:36 +0200

    Use same general error handler as spider chart

--- examples/ts-ganttchart-d3/src/index.ts
+++ examples/ts-ganttchart-d3/src/index.ts
@@ -45,7 +45,7 @@ Spotfire.initialize(async (mod) => {
      * It checks for valid data and will print errors in case of bad data or bad renders.
      * It calls the listener (reader) created earlier and adds itself as a callback to complete the loop.
      */
-    reader.subscribe(onChange);
+     reader.subscribe(generalErrorHandler(mod, 200)(onChange));
 
     /**
      * The function that is part of the main read-render loop.
@@ -62,27 +62,6 @@ Spotfire.initialize(async (mod) => {
         overdue: ModProperty<boolean>,
         weekend: ModProperty<boolean>
     ) {
-        try {
-            const errors = await dataView.getErrors();
-            if (errors.length > 0) {
-                mod.controls.errorOverlay.hide("General");
-                mod.controls.errorOverlay.show(errors.concat(messages.InitialConfigurationHelper), "DataView");
-                return;
-            }
-            mod.controls.errorOverlay.hide("DataView");
-
-            /**
-             * Hard abort if row count exceeds an arbitrary selected limit
-             */
-            const rowCount = await dataView.rowCount();
-            const limit = 20000;
-            if (rowCount && rowCount > limit) {
-                mod.controls.errorOverlay.show(
-                    `☹️ Cannot render - too many rows (rowCount: ${rowCount}, limit: ${limit}) `
-                );
-                return;
-            }
-
             /** Helper function to disregard duplicated rows on leaf nodes due to multiple links */
             const distinctRows = function (node: DataViewHierarchyNode): DataViewRow[] {
                 if (node.children) {
@@ -262,17 +241,6 @@ Spotfire.initialize(async (mod) => {
             context.signalRenderComplete();
 
             mod.controls.errorOverlay.hide("General");
-        } catch (e) {
-            mod.controls.errorOverlay.show(
-                [e.message].concat(messages.InitialConfigurationHelper) ||
-                    e ||
-                    "☹️ Something went wrong, check developer console",
-                "General"
-            );
-            if (DEBUG) {
-                throw e;
-            }
-        }
     }
 
     function getColor(node: DataViewHierarchyNode, root: DataViewHierarchyNode, isCategorical: boolean) {
@@ -320,3 +288,62 @@ Spotfire.initialize(async (mod) => {
         return color;
     }
 });
+
+
+
+/**
+ * subscribe callback wrapper with general error handling, row count check and an early return when the data has become invalid while fetching it.
+ *
+ * The only requirement is that the dataview is the first argument.
+ * @param mod - The mod API, used to show error messages.
+ * @param rowLimit - Optional row limit.
+ */
+ export function generalErrorHandler<T extends (dataView: Spotfire.DataView, ...args: any) => any>(
+    mod: Spotfire.Mod,
+    rowLimit = 2000
+): (a: T) => T {
+    return function (callback: T) {
+        return async function callbackWrapper(dataView: Spotfire.DataView, ...args: any) {
+            try {
+                const errors = await dataView.getErrors();
+                if (errors.length > 0) {
+                    mod.controls.errorOverlay.show(errors, "DataView");
+                    return;
+                }
+                mod.controls.errorOverlay.hide("DataView");
+
+                /**
+                 * Hard abort if row count exceeds an arbitrary selected limit
+                 */
+                const rowCount = await dataView.rowCount();
+                if (rowCount && rowCount > rowLimit) {
+                    mod.controls.errorOverlay.show(
+                        `☹️ Cannot render - too many rows (rowCount: ${rowCount}, limit: ${rowLimit}) `,
+                        "General"
+                    );
+                    return;
+                }
+
+                /**
+                 * User interaction while rows were fetched. Return early and respond to next subscribe callback.
+                 */
+                const allRows = await dataView.allRows();
+                if (allRows == null) {
+                    return;
+                }
+
+                await callback(dataView, ...args);
+
+                mod.controls.errorOverlay.hide("General");
+            } catch (e) {
+                mod.controls.errorOverlay.show(
+                    e.message || e || "☹️ Something went wrong, check developer console",
+                    "General"
+                );
+                if (DEBUG) {
+                    throw e;
+                }
+            }
+        } as T;
+    };
+}
