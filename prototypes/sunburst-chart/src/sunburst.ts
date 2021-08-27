c2272e2e100b1f9af9f787c6cfe3938a25d37ab1
Olof Bjerke <objerke@tibco.com>
2021-08-27 06:05:50 +0200

    cleanup

--- prototypes/sunburst-chart/src/sunburst.ts
+++ prototypes/sunburst-chart/src/sunburst.ts
@@ -39,6 +39,9 @@ export function render(
         .attr("id", "container")
         .attr("transform", "translate(" + size.width / 2 + "," + size.height / 2 + ")");
 
+    // Background circle for easier mouse leave detection.
+    vis.append("svg:circle").attr("r", radius).style("opacity", 0);
+
     document.querySelector("#mod-container svg")!.addEventListener("click", dataView.clearMarking);
 
     let partition = d3.partition().size([Math.PI * 2, radius]);
@@ -53,9 +56,6 @@ export function render(
 
     let partitionLayout = partition(hierarchy) as HierarchyRectangularNode<DataViewHierarchyNode>;
 
-    // Bounding circle underneath the sunburst, to make it easier to detect
-    // when the mouse leaves the parent g.
-    vis.append("svg:circle").attr("r", radius).style("opacity", 0);
 
     let path = vis
         .data([rootNode])
@@ -92,32 +92,23 @@ export function render(
 
             return d.data.rows()[firstMarkedRow].color().hexCode; /* colors[d.name]; */
         })
-        .on("mouseover", mouseover);
+        .on("mouseover", onMouseover);
 
-    // Add the mouseleave handler to the bounding circle.
-    d3.select("#container").on("mouseleave", mouseleave);
+    d3.select("#container").on("mouseleave", onMouseleave);
 
-    // Restore everything to full opacity when moving off the visualization.
-    function mouseleave(d: any) {
-        // Hide the breadcrumb trail
-        d3.select("#trail").style("visibility", "hidden");
-
-        // Deactivate all segments during transition.
+    function onMouseleave(d: any) {
         d3.selectAll("path").on("mouseover", null);
 
-        // Transition each segment to full opacity and then reactivate it.
         d3.selectAll("path")
             .transition()
-            .duration(1000)
+            .duration(500)
             .style("stroke", "transparent")
             .on("end", function () {
-                d3.select(this).on("mouseover", mouseover);
+                d3.select(this).on("mouseover", onMouseover);
             });
-
-        d3.select("#explanation").style("visibility", "hidden");
     }
 
-    function mouseover(d: any) {
+    function onMouseover(d: any) {
         let percentage = (100 * d.value) / totalSize;
         let percentageString = percentage.toPrecision(3) + "%";
         if (percentage < 0.1) {
@@ -127,24 +118,16 @@ export function render(
         d3.select("#percentage").text(percentageString);
         d3.select("#value").text(d.data.formattedValue());
 
-        d3.select("#explanation").style("visibility", "");
+        let ancestors = getAncestors(d);
 
-        let sequenceArray = getAncestors(d);
-
-        // Fade all the segments.
         d3.selectAll("path").style("stroke", "transparent");
 
-        // Then highlight only those that are an ancestor of the current segment.
         vis.selectAll("path")
-            .filter(function (node: any) {
-                return sequenceArray.indexOf(node) >= 0;
-            })
+            .filter((node: any) => ancestors.indexOf(node) >= 0)
             .style("stroke", "#888");
     }
 }
 
-// Given a node in a partition layout, return an array of all of its ancestor
-// nodes, highest first, but excluding the root.
 function getAncestors(node: d3.HierarchyNode<DataViewHierarchyNode>) {
     let path = [];
     let current = node;
