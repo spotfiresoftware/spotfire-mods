096c56f93061b4c5167a382b85c9a66b14aa63fe
Erik Brandin <ebrandin@tibco.com>
2021-09-09 23:39:07 +0200

    ColumnNames axis first produces well known order

--- prototypes/correlogram-chart/src/index.ts
+++ prototypes/correlogram-chart/src/index.ts
@@ -5,6 +5,7 @@ import { render, PairPlotData } from "./correlogram";
 
 const markerByAxisName = "MarkerBy";
 const measureNamesAxisName = "MeasureNames";
+const countAxisName = "Count";
 const colorAxisName = "Color";
 const measureAxisName = "Measures";
 
@@ -28,11 +29,7 @@ window.Spotfire.initialize(async (mod) => {
     let interaction = interactionLock();
     reader.subscribe(onChange);
 
-    async function onChange(
-        dataView: DataView,
-        measureAxis: Axis,
-        measureNamesAxis: Axis
-    ) {
+    async function onChange(dataView: DataView, measureAxis: Axis, measureNamesAxis: Axis) {
         if (measureAxis.parts.length < 2) {
             mod.controls.errorOverlay.show("Select two or more measures.", "Measures");
             return;
@@ -46,8 +43,7 @@ window.Spotfire.initialize(async (mod) => {
         }
 
         let error = await dataView.getErrors();
-        if (error.find(e => e.includes("basetrowid")))
-        {
+        if (error.find((e) => e.includes("baserowid"))) {
             showIdentifierSelection();
             return;
         }
@@ -56,49 +52,56 @@ window.Spotfire.initialize(async (mod) => {
 
         const measures = (await (await dataView.hierarchy(measureNamesAxisName))!.root())?.leaves();
         const columnNamesLeafIndices = measures?.map((l) => l.leafIndex) || [];
-        var rows = await dataView.allRows();
+        const rows = await dataView.allRows();
         if (!measures || !rows) {
             return;
         }
 
-        var points: (number | null)[][] = [];
-        var colors: string[] = [];
         var measureCount = columnNamesLeafIndices.length;
-        for (var i = 0; i < rows.length; i += measureCount) {
-            points.push(rows.slice(i, i + measureCount).map((r) => r.continuous(measureAxisName).value()));
-            colors.push(rows[i].color().hexCode)
-        }
+        var valueCount = rows.length / measureCount;
+        const points = transpose(
+            chunkArray(
+                rows.map((r) => r.continuous(measureAxisName).value()),
+                valueCount
+            )
+        );
+
+        // TODO Warn about column names on color.
+        // This is not possible since two different measures are combined into one element!
+        const colors = rows.slice(0, valueCount).map((r) => r.color().hexCode);
 
         var data: PairPlotData = {
             measures: measures.map((m) => m.formattedValue()),
             points,
             colors,
-            mark: (index: number) => rows![index * measureCount].mark()
+            mark: (index: number) => rows![index].mark()
         };
 
         render(data);
         context.signalRenderComplete();
     }
-
-    function showMeasureSelection()
-    {
-
+    function transpose(m: number[][]) {
+        return m[0].map((x, i) => m.map((x) => x[i]));
     }
+    function chunkArray(arr: any[], size: number) {
+        var results = [];
+        while (arr.length) {
+            results.push(arr.splice(0, size));
+        }
 
-    function showIdentifierSelection()
-    {
-
+        return results;
     }
+    function showMeasureSelection() {}
 
-    function resetMandatoryExpressions()
-    {
+    function showIdentifierSelection() {}
+
+    function resetMandatoryExpressions() {
         const div = document.getElementById("resetMandatoryExpressions")!;
-        div.style.display = "block"
+        div.style.display = "block";
         div.querySelector("button")?.addEventListener("click", () => {
             div.style.display = "none";
             mod.visualization.axis(measureNamesAxisName).setExpression("<[Axis.Default.Names]>");
-
-        })
-
+            mod.visualization.axis(countAxisName).setExpression("Count()");
+        });
     }
 });
