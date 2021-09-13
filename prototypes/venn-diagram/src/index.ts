d8137098b99af95003c393788937d04cc66e7b95
Olof Bjerke <objerke@tibco.com>
2021-09-13 16:17:50 +0200

    Add venn

--- prototypes/venn-diagram/src/index.ts
+++ prototypes/venn-diagram/src/index.ts
@@ -1,13 +1,11 @@
-import { DataView } from "spotfire-api";
+import { DataView, DataViewHierarchyNode, DataViewRow } from "spotfire-api";
 import { generalErrorHandler } from "./generalErrorHandler";
+import { createPairs } from "./pairs";
+import { Circle, render } from "./vennDiagram";
 
-const Spotfire = window.Spotfire;
+const axisName = "Color";
 
-export interface RenderState {
-    preventRender: boolean;
-}
-
-Spotfire.initialize(async (mod) => {
+window.Spotfire.initialize(async (mod) => {
     const context = mod.getRenderContext();
 
     const reader = mod.createReader(mod.visualization.data(), mod.windowSize());
@@ -23,8 +21,93 @@ Spotfire.initialize(async (mod) => {
      * @param {ModProperty<string>} curveType
      */
     async function onChange(dataView: DataView, windowSize: Spotfire.Size) {
-        console.log(dataView, windowSize);
-        
+        let circleRoot = await (await dataView.hierarchy(axisName))?.root();
+        let overlapRoot = await (await dataView.hierarchy("Overlap"))?.root();
+        let overlaps: { [pair: string]: Circle } = {};
+
+        // Create the initial circles.
+        for (let leaf of circleRoot!.leaves()) {
+            let k = leaf.leafIndex + "";
+            overlaps[k] = {
+                sets: [leaf.leafIndex!],
+                color: getColor(leaf),
+
+                label: leaf.formattedValue(),
+                tooltip: leaf.formattedValue(),
+                size: leaf.rowCount(),
+                rows: leaf.rows()
+            };
+        }
+
+        // Create all overlaps
+        for (const leaf of overlapRoot!.leaves()) {
+            let rows = leaf.rows();
+
+            if (rows.length < 2) {
+                continue;
+            }
+
+            const fullSet = leaf.rows().map((r) => r.categorical(axisName).leafIndex);
+
+            let sets: number[][] = createPairs(fullSet);
+
+            if (circleRoot!.leaves().length < 4) {
+                sets = [fullSet, ...sets];
+            }
+
+            for (const set of sets) {
+                let overlapKey = set.join("-");
+
+                overlaps[overlapKey] = overlaps[overlapKey] ?? {
+                    sets: set,
+                    size: 0,
+                    tooltip: rows.map((r) => r.categorical(axisName).formattedValue()).join("-"),
+                    rows: []
+                };
+
+                let rowsInSet = rows.filter((r) => set.includes(r.categorical(axisName).leafIndex));
+
+                overlaps[overlapKey].size++;
+                overlaps[overlapKey].rows.push(...rowsInSet);
+            }
+        }
+
+        let circles = Object.keys(overlaps).map((k) => overlaps[k]);
+
+        render(circles, {
+            size: windowSize,
+            onMouseover(d) {
+                mod.controls.tooltip.show(d.tooltip + ": " + d.size);
+            },
+            click(d) {
+                console.log(d);
+                if (!d) {
+                    dataView.clearMarking();
+                    return;
+                }
+
+                d.rows.map((r: DataViewRow) => r.mark());
+            },
+            getColor(set) {
+                let overlapKey = set.join("-");
+                return overlaps[overlapKey].color ?? "red";
+            }
+        });
+
         context.signalRenderComplete();
     }
 });
+
+function getColor(leaf: DataViewHierarchyNode) {
+    const rows = leaf.rows();
+    if (!rows.length) {
+        return "#ddd";
+    }
+
+    let firstMarkedRow = rows.findIndex((r) => r.isMarked());
+    if (firstMarkedRow == -1) {
+        firstMarkedRow = 0;
+    }
+
+    return rows[firstMarkedRow].color().hexCode;
+}
--- prototypes/venn-diagram/src/vennDiagram.ts
+++ prototypes/venn-diagram/src/vennDiagram.ts
@@ -1,6 +1,60 @@
 import * as d3 from "d3";
 
-export function render() {
-    const svg = d3.select("svg#svg");
-    console.log(svg);
+//@ts-ignore
+import * as venn from "./venn-lib";
+
+export interface Circle {
+    sets: number[];
+    color?: string;
+    label?: string;
+    tooltip?: string;
+    size: number;
+    rows: any;
+}
+
+interface Settings {
+    click(d: Circle | null): void;
+    size: { width: number; height: number };
+    onMouseover(d: Circle): void;
+    getColor(set: number[]): string;
+}
+
+export function render(circles: Circle[], settings: Settings) {
+    var chart = venn.VennDiagram().width(settings.size.width).height(settings.size.height).colours(settings.getColor);
+
+    var div = d3.select("#venn").on("click", () => settings.click(null));
+    div.selectAll("*").remove();
+    div.datum(circles).call(chart);
+
+    div.selectAll("path").style("stroke-opacity", 0).style("stroke", "#fff").style("stroke-width", 3);
+
+    div.selectAll<any, Circle>("g")
+        .on("mouseover", function (d: Circle, i) {
+            // sort all the areas relative to the current item
+            venn.sortAreas(div, d);
+
+            settings.onMouseover(d);
+
+            // highlight the current path
+            var selection = d3.select(this).transition("tooltip").duration(400);
+            selection
+                .select("path")
+                .style("fill-opacity", d.sets.length == 1 ? 0.4 : 0.1)
+                .style("stroke-opacity", 1);
+        })
+
+        .on("click", function (d: Circle, i) {
+            // sort all the areas relative to the current item
+            venn.sortAreas(div, d);
+            settings.click(d);
+            d3.event.stopPropagation();
+        })
+
+        .on("mouseout", function (d, i) {
+            var selection = d3.select(this).transition("tooltip").duration(400);
+            selection
+                .select("path")
+                .style("fill-opacity", d.sets.length == 1 ? 0.25 : 0.0)
+                .style("stroke-opacity", 0);
+        });
 }
