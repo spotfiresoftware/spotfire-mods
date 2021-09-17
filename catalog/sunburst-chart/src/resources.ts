c56f7a7a1ee9a4aa3e1dd689c1966e5fb5e28cc0
rebeccadahlman <60748346+rebeccadahlman@users.noreply.github.com>
2021-09-17 09:06:39 +0200

    Update resources.ts
    Updated some of the UI texts.

--- catalog/sunburst-chart/src/resources.ts
+++ catalog/sunburst-chart/src/resources.ts
@@ -1,16 +1,16 @@
 export const resources = {
-    warningEmptyPath: (a: string) => "Some segments are not visualized. The following path has holes in it: " + a,
-    warningNegativeValues: "The plot contains negative values. These segments have a red outline.",
+    warningEmptyPath: (a: string) => "Some segments are hidden because one or more levels in the hierarchy contain empty values: " + a,
+    warningNegativeValues: "Some segments represent negative values. These segments are shown with red outlines.",
     warningSplittingColorAxis:
         "The color expression generates more values than the hierarchy expression. Some leaves in the hierarchy will appear multiple times.",
     popoutLabelsAll: "All",
     popoutLabelsMarked: "Marked rows",
     popoutLabelsNone: "None",
-    popoutInteractionModeMark: "Mark",
-    popoutInteractionModeMarkTooltip: "Click segments to mark data. Use Alt + click for drill down mode.",
-    popoutInteractionModeDrillDown: "Drill down",
-    popoutInteractionModeDrillDownTooltip: "Click segments to drill down into the data. Use Alt + click for marking mode.",
-    popoutShowNullValues: "Display segments with empty values",
+    popoutInteractionModeMark: "Marking mode",
+    popoutInteractionModeMarkTooltip: "Click segments to mark data. To drill down instead, use Alt + click.",
+    popoutInteractionModeDrillDown: "Drill-down mode",
+    popoutInteractionModeDrillDownTooltip: "Click segments to drill down into the data. To mark segments instead, use Alt + click.",
+    popoutShowNullValues: "Show segments with empty values",
     popoutSortByValue: "Sort segments by value",
     warningsTooltip: (warnings: string[]) => "Warnings:\n" + warnings.join("\n"),
     breadCrumbRoot: "(All)"
