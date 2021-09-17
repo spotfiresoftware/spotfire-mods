133a5bf4cb97f87e212fd2fd8a872ff751075ddd
rebeccadahlman <60748346+rebeccadahlman@users.noreply.github.com>
2021-09-17 12:43:51 +0200

    Update resources.ts (#48)
    Changed popoutLabelsHeading.

--- catalog/rose-chart/src/resources.ts
+++ catalog/rose-chart/src/resources.ts
@@ -2,7 +2,7 @@ export const resources = {
     warningNegativeValues: "Some segments represent negative values. These segments are shown with red outlines.",
     warningsTooltip: (w: string[]) => "Warnings:\n" + w.join("\n"),
 
-    popoutLabelsHeading: "Labels",
+    popoutLabelsHeading: "Show labels for",
     popoutLabelsAll: "All",
     popoutLabelsMarked: "Marked rows",
     popoutLabelsNone: "None",
