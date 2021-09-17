572da6fdac0e218b428e20f541e53b559006ea68
Olof Bjerke <objerke@tibco.com>
2021-09-17 09:42:24 +0200

    Move resources into resources.ts

--- catalog/sunburst-chart/src/resources.ts
+++ catalog/sunburst-chart/src/resources.ts
@@ -3,11 +3,13 @@ export const resources = {
     warningNegativeValues: "The plot contains negative values. These segments have a red outline.",
     warningSplittingColorAxis:
         "The color expression generates more values than the hierarchy expression. Some leaves in the hierarchy will appear multiple times.",
+    popoutLabelsHeading: "Show labels for",
     popoutLabelsAll: "All",
     popoutLabelsMarked: "Marked rows",
     popoutLabelsNone: "None",
     popoutInteractionModeMark: "Mark",
     popoutInteractionModeMarkTooltip: "Click segments to mark data. Use Alt + click for drill down mode.",
+    popoutInteractionModeHeading: "Interaction",
     popoutInteractionModeDrillDown: "Drill down",
     popoutInteractionModeDrillDownTooltip: "Click segments to drill down into the data. Use Alt + click for marking mode.",
     popoutShowNullValues: "Display segments with empty values",
