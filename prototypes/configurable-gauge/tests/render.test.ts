d8a72e7fb86cb66911b10bb142fc3be8a0454c6c
Olof Bjerke <objerke@tibco.com>
2021-10-16 12:22:48 +0200

    add test

--- prototypes/configurable-gauge/tests/render.test.ts
+++ prototypes/configurable-gauge/tests/render.test.ts
@@ -36,4 +36,62 @@ describe("Render", () => {
         expect(svg.attr("width")).toBe("70");
         expect(svg.attr("height")).toBe("40");
     });
+
+    it("Renders a group per gauge", async () => {
+        const svg = d3.select("body").append("svg").attr("xmlns", "http://www.w3.org/2000/svg");
+
+        render(
+            [
+                {
+                    color: "red",
+                    formattedValue: "10 000",
+                    key: "first",
+                    label: "First",
+                    mark() {},
+                    maxLabel: "20 000",
+                    minLabel: "0",
+                    percent: 0.5
+                }
+            ],
+            {
+                svg: svg,
+                style: defaultStyling,
+                size: { width: 70, height: 40 }
+            }
+        );
+
+        expect(svg.selectAll("g.gauge").size()).toBe(1);
+
+        render(
+            [
+                {
+                    color: "red",
+                    formattedValue: "10 000",
+                    key: "first",
+                    label: "First",
+                    mark() {},
+                    maxLabel: "20 000",
+                    minLabel: "0",
+                    percent: 0.5
+                },
+                {
+                    color: "green",
+                    formattedValue: "10 000",
+                    key: "second",
+                    label: "Second",
+                    mark() {},
+                    maxLabel: "20 000",
+                    minLabel: "0",
+                    percent: 0.5
+                }
+            ],
+            {
+                svg: svg,
+                style: defaultStyling,
+                size: { width: 70, height: 40 }
+            }
+        );
+
+        expect(svg.selectAll("g.gauge").size()).toBe(2);
+    });
 });
