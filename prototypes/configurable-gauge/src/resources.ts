04906bc9319bd47fb8fe9e728095a00953127277
Olof Bjerke <objerke@tibco.com>
2021-10-18 13:23:23 +0200

    Add adjustable pad angle

--- prototypes/configurable-gauge/src/resources.ts
+++ prototypes/configurable-gauge/src/resources.ts
@@ -4,6 +4,7 @@ export const resources = {
     warningMinEqualToMax: (label: string, minValue: any, maxValue: any) =>
         `${label} has a min value (${minValue}) that is identical to its max value (${maxValue})`,
     settingArcWidth: "Arc width:",
+    settingAngle: "Gauge angle:",
     settingsBackgroundOpacity: "Background opacity:",
     settingsScaleTicksOpacity: "Scale ticks opacity:",
     settingsShowPercent: "Show percent:",
