107e818b851bba26e8b09d550c9a2690187f8fe4
tobiashjalmarsson <tobhja@student.chalmers.se>
2022-07-18 11:48:46 +0200

    Updating package.json with new author

--- prototypes/funnel-chart/src/resources.ts
+++ prototypes/funnel-chart/src/resources.ts
@@ -5,10 +5,10 @@ export const resources = {
     popoutLabelsNone: "None",
     popoutSortByValue: "Sort segments by value",
     warningsTooltip: (warnings: string[]) => "Warnings:\n" + warnings.join("\n"),
-    showInLabelName : "X value",
-    showInLabelQuantity : "Y value",
-    showInLabelPercentage : "Y value (percentage)",
-    showLabelInFunnel : "Inside Funnel",
-    showLabelOutsideFunnel : "Outside Funnel",
-    showOnlyMarkedProcesses : "Only display marked processes"
+    showInLabelName: "X value",
+    showInLabelQuantity: "Y value",
+    showInLabelPercentage: "Y value (percentage)",
+    showLabelInFunnel: "Inside Funnel",
+    showLabelOutsideFunnel: "Outside Funnel",
+    showOnlyMarkedProcesses: "Only display marked processes"
 };
