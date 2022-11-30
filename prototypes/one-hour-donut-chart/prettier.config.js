62fce1514715698b32f76f25085eec09f05e88fb
Erik Brandin <ebrandin@tibco.com>
2022-11-30 14:09:50 +0100

    Re-branding
    Updating license file
    
    Updating copyright headers
    
    Revert "Fixed issue for no color axis expression."
    
    This reverts commit 08f1f2bb5294c80cc32458265351df967ba2b442.

--- prototypes/one-hour-donut-chart/prettier.config.js
+++ prototypes/one-hour-donut-chart/prettier.config.js
@@ -1,5 +1,5 @@
 /*
-* Copyright © 2020. TIBCO Software Inc.
+* Copyright © 2023. Cloud Software Group, Inc.
 * This file is subject to the license terms contained
 * in the license file that is distributed with this file.
 */
--- tools/mods-dev-server/cli.js
+++ tools/mods-dev-server/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
- * Copyright © 2020. TIBCO Software Inc.
+ * Copyright © 2023. Cloud Software Group, Inc.
  * This file is subject to the license terms contained
  * in the license file that is distributed with this file.
  */
