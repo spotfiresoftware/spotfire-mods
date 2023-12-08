f15f7a1f4592610e209bd9633af0d6fdf949b7d8
Henrik Andersson <henrik.andersson@cloud.com>
2023-12-08 11:12:44 +0100

    Add option to allow fetch of project root path

--- tools/mods-dev-server/server.js
+++ tools/mods-dev-server/server.js
@@ -52,7 +52,8 @@ const defaultSettings = {
     port: 8090,
     open: true,
     root: ".",
-    path: "/" + manifestName
+    path: "/" + manifestName,
+    allowProjectRoot: false
 };
 
 module.exports.start = start;
@@ -91,6 +92,17 @@ function start(settings = {}) {
     app.use(cacheHeaders);
     app.use(cspHeaders);
     app.use(corsHeaders);
+
+    if (settings.allowProjectRoot) {
+        // We need to be able to retrieve the absolute path to the project root to
+        // enable source maps when debugging scripts in action mods.
+        app.use("/modProjectRoot", (req, res, next) => {
+            res.setHeader("Content-Type", "text/plain; charset=UTF-8");
+            res.write(rootDirectoryAbsolutePath);
+            res.end();
+        });
+    }
+
     app.use(checkIfPartOfManifest);
     app.use(onlyWhenOriginIsSet(injectWebSocketSnippet(settings)));
     app.use(serveStaticFiles);
